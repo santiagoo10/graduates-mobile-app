af4e269efed8ae2c214b8ccee39d71be34782e87
Santiago Bolzico <sgobolzico@mercadolibre.com>
2023-05-20 14:04:04 -0300

    add screens names utils

---
+++ src/utils/screenName.js
@@ -0,0 +1,24 @@
+const saleStack = {
+    tab: "sale-list-tab",
+    saleList: "sale-list",
+    saleAdd: "sale-list",
+    saleDetail: "sale-detail",
+  };
+  
+  const mapStack = {
+    tab: "map-list",
+    mapDetail: "map-detail",
+  };
+  
+  const accountStack = {
+    tab: "account-tab",
+    account: "acount",
+    login: "login",
+    register: "register",
+  };
+  
+  export const screen = {
+    sale: saleStack,
+    map: mapStack,
+    account: accountStack,
+  };
