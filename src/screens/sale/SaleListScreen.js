ab08ab094f3a610ccb68438360eeea42bd898052
Santiago Bolzico <sgobolzico@mercadolibre.com>
2023-05-21 10:29:34 -0300

    add nagitions into stack

--- src/screens/sale/SaleListScreen.js
+++ src/screens/sale/SaleListScreen.js
@@ -1,10 +1,19 @@
 import { View, Text } from "react-native";
+import { Button } from "react-native-elements";
 import React from "react";
+import { screen } from "../../utils";
 
-export function SaleListScreen() {
+export function SaleListScreen(props) {
+  const { navigation } = props;
+
+  const goToAdd = () => {
+    navigation.navigate(screen.sale.tab, { screen: screen.sale.saleAdd });
+  };
+
   return (
     <View>
       <Text>SaleListScreen</Text>
+      <Button title="Agregar beneficio" onPress={goToAdd} />
     </View>
   );
 }
