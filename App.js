d1e9ac82b52ebe792c338e6468c7fde41aadc29c
Santiago Bolzico <sgobolzico@mercadolibre.com>
2023-05-28 18:11:49 -0300

    add login account

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { AppNavigation } from "./src/navigation/AppNavigation";
+import { initFirebase } from "./src/utils";
 
 export default function App() {
   return (
--- src/screens/account/loginScreen/LoginScreen.js
+++ src/screens/account/loginScreen/LoginScreen.js
@@ -1,10 +1,35 @@
-import { View, Text } from "react-native";
 import React from "react";
+import { View, ScrollView } from "react-native";
+import { Text, Image } from "react-native-elements";
+import { useNavigation } from "@react-navigation/native";
+// import { LoginForm } from "../../../components/Auth";
+import { screen } from "../../../utils";
+import { styles } from "./LoginScreen.styles";
 
 export function LoginScreen() {
+  const navigation = useNavigation();
+
+  const goToRegister = () => {
+    navigation.navigate(screen.account.register);
+  };
+
   return (
-    <View>
-      <Text>LoginScreen</Text>
-    </View>
+    <ScrollView>
+      <Image
+        source={require("../../../../assets/logo-unl-3.png")}
+        style={styles.logo}
+        resizeMode="contain"
+      />
+      <View style={styles.viewContainer}>
+        {/* <LoginForm /> */}
+
+        <Text style={styles.textRegister}>
+          ¿Aún no tienes cuenta{" "}
+          <Text style={styles.btnRegister} onPress={goToRegister}>
+            Regístrarse
+          </Text>
+        </Text>
+      </View>
+    </ScrollView>
   );
 }
